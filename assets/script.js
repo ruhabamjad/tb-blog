486d8db407319f7398b69aff6f0e7ce9ef494846
Ruhab Amjad <ruhabrj23@gmail.com>
2024-05-08 14:31:28 +0500

    Added TB Blog Files

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+// Navbar Menu
+let collapse = document.getElementById("collapse");
+let myUl = document.getElementById("my-ul");
+
+collapse.addEventListener("click", () => {
+    myUl.classList.toggle("left");
+});
